791a33b27a3b26aadc117c335c4a6c053d606419
moinul011231 <moinul4039@gmail.com>
2023-11-07 00:30:23 +0600

    skeleton 2

---
+++ social-spa/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Social Apps';
+  users: any;
+    constructor(private http:HttpClient){}
+
+    ngOnInit(): void{
+      this.http.get<any>('https://localhost:5001/api/users').subscribe({
+        next:(res) => {
+          this.users = res;
+          console.log(res);
+        },error: (err) => {
+          console.log(err);
+        },complete:() =>{
+          console.log('Request Completed');
+        }
+      })
+    }
+  
+}
