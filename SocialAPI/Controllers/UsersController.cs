685f37667a259f7591f9fa5a1ceb50b472e63a7e
moinul011231 <moinul4039@gmail.com>
2023-11-06 15:28:47 +0600

    Skeleton create 1

---
+++ SocialAPI/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialAPI.TData;
+using SocialAPI.TEntities;
+
+namespace SocialAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly TDataContext _context;
+        public UsersController(TDataContext context)
+        {
+            _context = context;
+        }
+
+        //[HttpGet]
+        //public ActionResult<IEnumerable<AppUser>> GetUser()
+        //{
+        //    var users = _context.Users.ToList();
+        //    return Ok(users);
+        //}
+
+
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        //[HttpGet("id")]
+        //public ActionResult<AppUser> GetUserById(int id)
+        //{
+        //    var user = _context.Users.Find(id);
+        //    return Ok(user);
+        //}
+
+
+        [HttpGet("id")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return Ok(user);
+        }
+    }
+}
---
+++ SocialAPI/TData/TDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialAPI.TEntities;
+
+namespace SocialAPI.TData
+{
+    public class TDataContext : DbContext
+    {
+        public TDataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
